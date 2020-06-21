6257b745ef3e599be68845525cbaccd723f64896
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-21 18:56:16 +0300

    finnished the backedn

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,3 +1,26 @@
 const route = require("express").Router();
+const Todo = require("../models/Todo");
+
+route.post("/", async (req, res) => {
+  try {
+    const { text } = req.body;
+    const todo = new Todo({
+      body: text
+    });
+    await todo.save();
+    res.send(todo);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+route.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Todo.findByIdAndDelete(id);
+    res.send({ message: "Deleted successfully!" });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 module.exports = route;
